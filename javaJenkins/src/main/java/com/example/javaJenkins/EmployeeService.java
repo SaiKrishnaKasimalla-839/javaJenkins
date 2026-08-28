65b9b9798630aa93c98c1b11040c101817ab5573
root <root@ip-175-10-8-212.us-east-2.compute.internal>
2026-08-28 11:07:21 +0000

    sourceCode

---
+++ javaJenkins/src/main/java/com/example/javaJenkins/EmployeeService.java
@@ -0,0 +1,32 @@
+
+package com.example.javaJenkins;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
